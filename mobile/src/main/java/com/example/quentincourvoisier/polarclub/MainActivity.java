09a9aae426818df1b37d262c3cba63baf79a335e
SylvainMacabrey <sylvian.macabrey@edu.univ-fcomte.fr>
2018-01-31 18:59:26 +0100

    Menu fonctionnel module : mobile

--- mobile/src/main/java/com/example/quentincourvoisier/polarclub/MainActivity.java
+++ mobile/src/main/java/com/example/quentincourvoisier/polarclub/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.quentincourvoisier.polarclub;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import com.example.quentincourvoisier.polarclub.fragments.AddSessionFragment;
+import com.example.quentincourvoisier.polarclub.fragments.HomeFragment;
+import com.example.quentincourvoisier.polarclub.fragments.ListSessionFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +21,22 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    setTitle("Home");
+                    HomeFragment homeFragment = new HomeFragment();
+                    FragmentTransaction homeFragmentTransaction = getFragmentManager().beginTransaction();
+                    homeFragmentTransaction.replace(R.id.content, homeFragment, "FragmentName").commit();
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_new_session:
+                    setTitle("New session");
+                    AddSessionFragment sessionFragment = new AddSessionFragment();
+                    FragmentTransaction addSessionFragmentTransaction = getFragmentManager().beginTransaction();
+                    addSessionFragmentTransaction.replace(R.id.content, sessionFragment, "FragmentName").commit();
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_list_session:
+                    setTitle("List Sessions");
+                    ListSessionFragment listSessionFragment = new ListSessionFragment();
+                    FragmentTransaction listSessionFragmentTransaction = getFragmentManager().beginTransaction();
+                    listSessionFragmentTransaction.replace(R.id.content, listSessionFragment, "FragmentName").commit();
                     return true;
             }
             return false;
@@ -36,9 +48,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        setTitle("Home");
+        HomeFragment homeFragment = new HomeFragment();
+        FragmentTransaction homeFragmentTransaction = getFragmentManager().beginTransaction();
+        homeFragmentTransaction.replace(R.id.content, homeFragment, "FragmentName").commit();
     }
 
 }
