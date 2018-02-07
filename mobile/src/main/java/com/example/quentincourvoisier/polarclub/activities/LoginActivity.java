bf14415a6e3efa29bb48b2a6f68fd6ad05955608
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-02-07 19:21:49 +0100

    Système pour ce login

--- mobile/src/main/java/com/example/quentincourvoisier/polarclub/activities/LoginActivity.java
+++ mobile/src/main/java/com/example/quentincourvoisier/polarclub/activities/LoginActivity.java
@@ -1,15 +1,103 @@
 package com.example.quentincourvoisier.polarclub.activities;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.quentincourvoisier.polarclub.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String BUTTON_SUBMIT = "button_submit";
+
+    private EditText emailField;
+    private EditText passwordField;
+    private Button buttonSubmit;
+
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailField = findViewById(R.id.loginAct_email);
+        passwordField = findViewById(R.id.loginAct_password);
+        buttonSubmit = findViewById(R.id.loginAct_submit);
+
+        buttonSubmit.setTag(BUTTON_SUBMIT);
+        buttonSubmit.setOnClickListener(this);
+
+        auth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() != null) {
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getTag().equals(BUTTON_SUBMIT)) {
+            startSignIn();
+        }
+    }
+
+    private void startSignIn() {
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
+
+        if (verif(email, password)) {
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Sign in problem", Toast.LENGTH_SHORT).show();
+                    } else {
+                        String message = "Email = " + task.getResult().getUser().getEmail();
+                        Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private boolean verif(String email, String password) {
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Email not empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (!email.contains("@")) {
+            Toast.makeText(this, "Email is not valid", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Password not empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
     }
 }
---
+++ mobile/src/main/java/com/example/quentincourvoisier/polarclub/firebase/FireApp.java
@@ -0,0 +1,17 @@
+package com.example.quentincourvoisier.polarclub.firebase;
+
+import android.app.Application;
+
+import com.firebase.client.Firebase;
+
+/**
+ * Created by antho on 07/02/2018.
+ */
+
+public class FireApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
